982b20c437fd415ee3b5dc3c60ca67d56a74c282
surahj <odebodeopeyemi@gmail.com>
2022-09-30 23:36:35 +0100

    Fix error

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -19,6 +19,6 @@ echo "<html>
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data
 sudo chmod -R 755 /data/
-sudo sed -i '19 i\\n    location /hbnb_static/ {\n        alias /data/web_static/current/;\n    }\n' /etc/nginx/sites-available/default
+sudo sed -i '48 i \\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
 sudo service nginx restart
 
