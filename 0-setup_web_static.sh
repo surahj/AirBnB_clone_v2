e934e2caa9cd222e119b7e56737c04dc4da589c1
surahj <odebodeopeyemi@gmail.com>
2022-09-30 23:45:59 +0100

    Fix error

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,24 +1,35 @@
 #!/usr/bin/env bash
-# Bash script that sets up your web servers for the deployment of web_static
-# Install Nginx if it not already installed
-if [ ! -x /usr/sbin/nginx ]
-then
-    sudo apt-get -y update
-    sudo apt-get -y install nginx
-fi
+# Sets up a web server for deployment of web_static.
+
+sudo apt-get update
+sudo apt-get install -y nginx
+
 sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared/
-sudo touch /data/web_static/releases/test/index.html
-echo "<html>
-  <head>
-  </head>
-  <body>
-    <h1> Holberton School <h1>
-  </body>
-</html>" | sudo tee /data/web_static/releases/test/index.html
+echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo chown -R ubuntu:ubuntu /data
-sudo chmod -R 755 /data/
-sudo sed -i '48 i \\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
-sudo service nginx restart
 
+sudo chown -R ubuntu /data/
+sudo chgrp -R ubuntu /data/
+
+printf %s "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By $HOSTNAME;
+    root   /var/www/html;
+    index  index.html index.htm;
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
+    }
+    location /redirect_me {
+        return 301 http://cuberule.com/;
+    }
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
+    }
+}" | sudo tee /etc/nginx/sites-available/default
+
+sudo service nginx restart
