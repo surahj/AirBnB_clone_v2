dcdabca21e85e606568dfe4f29af94320925c0b6
surahj <odebodeopeyemi@gmail.com>
2022-09-30 22:01:29 +0100

    Fix error

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -6,10 +6,8 @@ then
     sudo apt-get -y update
     sudo apt-get -y install nginx
 fi
-# Create the folders
 sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared/
-# Create a fake HTML file
 sudo touch /data/web_static/releases/test/index.html
 echo "<html>
   <head>
@@ -18,13 +16,11 @@ echo "<html>
      Holberton School
   </body>
 </html>" | sudo tee /data/web_static/releases/test/index.html
-# Create a symbolic link /data/web_static/current linked to the /data/web_static/releases/test/ folder
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-# Give ownership of the /data/ folder to the ubuntu user AND group 
 sudo chown -R ubuntu:ubuntu /data
 sudo chmod -R 755 /data/
-# Update the Nginx configuration to serve the content of /data/web_static/current/ to hbnb_static
+
 sudo sed -i '19 i\\n    location /hbnb_static/ {\n        alias /data/web_static/current/;\n    }\n' /etc/nginx/sites-available/default
-# Restart nginx
+
 sudo service nginx restart
 
