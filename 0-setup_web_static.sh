b700aacf12c8fab92868489d6a4f43c06664bbca
surahj <odebodeopeyemi@gmail.com>
2022-09-30 20:28:52 +0100

    Create 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -10,12 +10,12 @@ fi
 sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared/
 # Create a fake HTML file
-touch /data/web_static/releases/test/index.html
-echo "<html>
+sudo touch /data/web_static/releases/test/index.html
+sudo echo "<html>
   <head>
   </head>
   <body>
-    <h1>Testing Nginx configuration <h1>
+    <h1> Holberton School <h1>
   </body>
 </html>" > /data/web_static/releases/test/index.html
 # Create a symbolic link /data/web_static/current linked to the /data/web_static/releases/test/ folder
@@ -24,6 +24,7 @@ sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data
 sudo chmod -R 755 /data/
 # Update the Nginx configuration to serve the content of /data/web_static/current/ to hbnb_static
-sudo sed -i '48 i \\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+sudo sed -i '19 i\\n    location /hbnb_static/ {\n        alias /data/web_static/current/;\n    }\n' /etc/nginx/sites-available/default
 # Restart nginx
 sudo service nginx restart
+
